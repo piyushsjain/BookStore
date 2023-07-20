d5d3cd4095140f5654753b75c18f840e59226e1b
piyushsjain <piyushjain31082000@gmail.com>
2023-07-20 09:03:00 +0530

    Added View, Controller and Models to application at basic level.

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBooks(string title, string authorName)  
+        {
+            return DataSource().Where(x => x.Title == title && x.Author == authorName).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Title="MVC",Author="Nitish"},
+                new BookModel(){Id=2, Title="Java",Author="Nitish"},
+                new BookModel(){Id=3, Title="C#",Author="Monika"},
+                new BookModel(){Id=4, Title="Dotnet",Author="Webgentle"},
+                new BookModel(){Id=5, Title="Angular",Author="Harshs"}
+            };
+        }
+    }
+}
